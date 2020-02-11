383a74702fd1d412862da5e70f5eb57572104145
hp <hp@DESKTOP-3JNFKFN>
2020-02-11 12:53:05 +0530

    Bubble sort

---
+++ src/sorting/Bubblesort1.java
@@ -0,0 +1,32 @@
+package sorting;	
+class BubbleSort1
+{
+	public static void main(String args[])
+    {
+        BubbleSort1 ob = new BubbleSort1();
+        int arr[] = {12, 23, 25, 19, 24, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
+
